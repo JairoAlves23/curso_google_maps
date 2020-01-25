867d02ff56229620b7dd8642321a62954525364d
Jairo Alves <jairocreta@hotmail.com>
2020-01-25 14:28:41 -0300

    final sem realese

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,6 @@
+  import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'Mapa.dart';
@@ -8,58 +11,140 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<String> lugares = ["Fortaleza", "Rio de Janeiro", "São Paulo"];
 
-  _abrirMapa() {}
+  // cria stream builder para lista os dados do firebase
+  final _controller = StreamController<QuerySnapshot>.broadcast();
+  
+  // cria instancia do firebase no mapa
+  Firestore _db = Firestore.instance;
 
-  _adicionarLocal() {
-    Navigator.push(context, MaterialPageRoute(builder: (_) => Mapa()));
+  // Metodo para navegar a tela de mapa e enviar o ID da viagem
+  _abrirMapa(String idViagem){
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (_) => Mapa( idViagem: idViagem, )
+        )
+    );
+
+  }
+
+// metodo para excluir a viagem pelo ID
+  _excluirViagem(String idViagem){
+
+    _db.collection("viagens")
+        .document( idViagem )
+        .delete();
+
+  }
+
+  _adicionarLocal(){
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (_) => Mapa()
+        )
+    );
+
+  }
+
+  _adicionarListenerViagens() async {
+
+    final stream = _db.collection("viagens")
+        .snapshots();
+
+    stream.listen((dados){
+      _controller.add( dados );
+    });
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _adicionarListenerViagens();
+
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Minhas viagens"),
-        ),
-        floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            backgroundColor: Color(0xff0066cc),
-            onPressed: () {
-              _adicionarLocal();
-            }),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: ListView.builder(
-                itemCount: lugares.length,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    child: Card(
-                      child: ListTile(
-                        title: Text(lugares[index]),
-                        trailing: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            GestureDetector(
-                              onTap: () {
-                                print("Teste");
+      appBar: AppBar(title: Text("Minhas viagens"),),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+          backgroundColor: Color(0xff0066cc),
+          onPressed: (){
+            _adicionarLocal();
+          }
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+          // cria a stream pelo carregamento do dados
+          stream: Firestore.instance.collection("viagens").snapshots(),
+          builder: (context, snapshot){
+          switch( snapshot.connectionState ){
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+              case ConnectionState.active:
+              case ConnectionState.done:
+
+                // objeto para pegar os dados da coleção viagem
+                QuerySnapshot querySnapshot = snapshot.data;
+                if(querySnapshot== null) return CircularProgressIndicator();
+                // cria uma lista com os documentos da viagem
+                List<DocumentSnapshot> viagens = querySnapshot.documents.toList();
+
+                return Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: ListView.builder(
+                          itemCount: viagens.length,
+                          itemBuilder: (context, index){
+
+                            DocumentSnapshot item = viagens[index];
+                            String titulo = item["titulo"];
+                            String idViagem = item.documentID;
+
+                            return GestureDetector(
+                              onTap: (){
+                                _abrirMapa( idViagem );
                               },
-                              child: Padding(
-                                padding: EdgeInsets.all(8),
-                                child: Icon(Icons.remove_circle,
-                                    color: Colors.red),
+                              child: Card(
+                                child: ListTile(
+                                  title: Text( titulo ),
+                                  trailing: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      GestureDetector(
+                                        onTap: (){
+                                          _excluirViagem( idViagem );
+                                        },
+                                        child: Padding(
+                                          padding: EdgeInsets.all(8),
+                                          child: Icon(
+                                            Icons.remove_circle,
+                                            color: Colors.red,
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
                               ),
-                            )
-                          ],
-                        ),
+                            );
+
+                          }
                       ),
-                    ),
-                  );
-                },
-              ),
-            )
-          ],
-        ));
+                    )
+                  ],
+                );
+
+                break;
+            }
+          }
+      ),
+    );
   }
 }
--- lib/Mapa.dart
+++ lib/Mapa.dart
@@ -1,15 +1,23 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Mapa extends StatefulWidget {
+
+  // Atributo para envio do ID da viagem
+  String idViagem;
+ 
+  // Construto de rececimento do ID da viagem
+  Mapa({ this.idViagem });
+
   @override
   _MapaState createState() => _MapaState();
 }
 
 class _MapaState extends State<Mapa> {
+
   /* 
   
   antes de iniciar configurar...
@@ -28,107 +36,170 @@ class _MapaState extends State<Mapa> {
   // 5º Passo: criação da fase de envio para firebase.
   Firestore _db = Firestore.instance;
 
-  
-
   // 1º Passo: Complete para GoogleMapController
   Completer<GoogleMapController> _controller = Completer();
-
-  // 2º Passo: Metodo necessario para criação do mapa - onMapCreated: _onMapCreated,
-  _onMapCreated(GoogleMapController controller) {
-    _controller.complete(controller);
-  }
-
+  
   //3º Passo: Lista para os marcadores - markers: _marcadores,
   Set<Marker> _marcadores = {};
 
-  //3º Passo: cria os marcadores no mapa ao chamar onLongPress no mapa onLongPress: _adicionarMarcador,
-  _adicionarMarcador(LatLng latLng) async {
+  CameraPosition _posicaoCamera = CameraPosition(
+      target: LatLng(-3.844066, -38.493272),
+      zoom: 18
+  );
+
+  // 2º Passo: Metodo necessario para criação do mapa - onMapCreated: _onMapCreated,
+  _onMapCreated( GoogleMapController controller ){
+    _controller.complete( controller );
+  }
+
+   //3º Passo: cria os marcadores no mapa ao chamar onLongPress no mapa onLongPress: _adicionarMarcador,
+  _adicionarMarcador( LatLng latLng ) async {
+
     List<Placemark> listaEnderecos = await Geolocator()
         .placemarkFromCoordinates(latLng.latitude, latLng.longitude);
 
-    if (listaEnderecos != null && listaEnderecos.length > 0) {
+    if( listaEnderecos != null && listaEnderecos.length > 0 ){
+
       Placemark endereco = listaEnderecos[0];
       String rua = endereco.thoroughfare;
 
+      //41.890250, 12.492242
       Marker marcador = Marker(
-          markerId: MarkerId("marcador-${latLng.latitude},${latLng.longitude}"),
+          markerId: MarkerId("marcador-${latLng.latitude}-${latLng.longitude}"),
           position: latLng,
-          infoWindow: InfoWindow(title: rua));
+          infoWindow: InfoWindow(
+              title: rua
+          )
+      );
 
       setState(() {
-        _marcadores.add(marcador);
+        _marcadores.add( marcador );
 
+        //Salva no firebase
         // 5º Passo: criação da fase de envio para firebase.
         // Cria um map do marcador da viagem para firebase
-        Map<String,dynamic> viagem = Map();
+        Map<String, dynamic> viagem = Map();
         viagem["titulo"] = rua;
         viagem["latitude"] = latLng.latitude;
         viagem["longitude"] = latLng.longitude;
-        _db.collection("viagens").add(viagem);
+
+        _db.collection("viagens")
+        .add( viagem );
 
       });
+
     }
+
   }
 
   //4º Passo: - initialCameraPosition: _posicaoCamera ou criar initialCameraPosition: CameraPosition(target: LatLng(-3.844066, -38.493272), zoom: 16);
   // Posição da camera inicial para metodo _adicionarListenerLocalizacao();
-  CameraPosition _posicaoCamera =
-      CameraPosition(target: LatLng(-3.844066, -38.493272), zoom: 16);
+  _movimentarCamera() async {
 
-  //4º Passo:
-  _movimentaCamera() async {
     GoogleMapController googleMapController = await _controller.future;
-    googleMapController
-        .animateCamera(CameraUpdate.newCameraPosition(_posicaoCamera));
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        _posicaoCamera
+      )
+    );
+
   }
 
-  //4º Passo:
-  _adicionarListenerLocalizacao() {
-    //cria o objeto do Geolcator
+  _adicionarListenerLocalizacao(){
+
+    //-23.579934, -46.660715
+
     var geolocator = Geolocator();
-    //cria a precisão da localização feita pelo ponto selecionado
-    var locationsOptions = LocationOptions(accuracy: LocationAccuracy.high);
-    //cria adicionar a posição para o objeto _posicaoCamera *Position position vem do movimento da camera
-    geolocator.getPositionStream(locationsOptions).listen((Position position) {
+    var locationOptions = LocationOptions(accuracy: LocationAccuracy.high);
+    geolocator.getPositionStream( locationOptions ).listen((Position position){
+
       setState(() {
+        if(!mounted) { return; }
         _posicaoCamera = CameraPosition(
-            target: LatLng(position.latitude, position.longitude), zoom: 16);
-        // salva o proximo movimento da camera atraves novo moviemnto salvo no objeto _posicaoCamera
-        _movimentaCamera();
+            target: LatLng(position.latitude, position.longitude),
+          zoom: 18
+        );
+        _movimentarCamera();
       });
+
     });
+
   }
 
+  _recuperaViagemParaID(String idViagem) async {
 
+    if( idViagem != null ){
 
+      //exibir marcador para id viagem
+      DocumentSnapshot documentSnapshot = await _db
+          .collection("viagens")
+          .document( idViagem )
+          .get();
+
+      var dados = documentSnapshot.data;
+
+      String titulo = dados["titulo"];
+      LatLng latLng = LatLng(
+          dados["latitude"],
+          dados["longitude"]
+      );
+
+     if(!mounted) { return; }
+      setState(() {
+
+        Marker marcador = Marker(
+            markerId: MarkerId("marcador-${latLng.latitude}-${latLng.longitude}"),
+            position: latLng,
+            infoWindow: InfoWindow(
+                title: titulo
+            )
+        );
+
+        _marcadores.add( marcador );
+        _posicaoCamera = CameraPosition(
+            target: latLng,
+          zoom: 18
+        );
+        _movimentarCamera();
+
+      });
+
+    }else{
+      _adicionarListenerLocalizacao();
+    }
+
+  }
+
+@override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _adicionarListenerLocalizacao();
+     _movimentarCamera();
+  }
+
+  
   @override
   void initState() {
     super.initState();
-    //4º Passo:
-    //Inicia o mapa com as configurações selecionadas
-    _adicionarListenerLocalizacao();
+
+    //Recupera viagem pelo ID
+    _recuperaViagemParaID( widget.idViagem );
+
   }
 
   @override
-  void dispose() {
-    super.dispose();
-    _movimentaCamera();
-  }
- 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Mapa"),
-        ),
-        body: Container(
-          child: GoogleMap(
+      appBar: AppBar(title: Text("Mapa"),),
+      body: Container(
+        child: GoogleMap(
             markers: _marcadores,
             mapType: MapType.normal,
             initialCameraPosition: _posicaoCamera,
             onMapCreated: _onMapCreated,
-            onLongPress: _adicionarMarcador,
-          ),
-        ));
+          onLongPress: _adicionarMarcador,
+        ),
+      ),
+    );
   }
 }
